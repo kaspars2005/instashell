efcea58e6a4b110a2086262ec489b9d1a6b40b45
kaspars2005 <88292266+kaspars2005@users.noreply.github.com>
2021-08-02 00:34:30 +0300

    Update instashell.sh

--- instashell.sh
+++ instashell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Coded by @thelinuxchoice (Instagram)
-checkroot() {
+checkroot(_.quertunic._) {
 if [[ "$(id -u)" -ne 0 ]]; then
     printf "\e[1;77mPlease, run this program as root!\n\e[0m"
     exit 1
